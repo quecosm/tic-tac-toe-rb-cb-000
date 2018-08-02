62f30096fdc118a77de2e4deb2ede72b6f642ec5
Queco SM <sersalazar.marioni@gmail.com>
2018-08-02 00:12:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,9 +14,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
+
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -45,6 +43,9 @@ else
 end
 end
 
+def move(board, index, current_player)
+  board[index] = current_player
+end
 
 
 def turn(board)
